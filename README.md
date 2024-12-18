a47b30d28b48dfe598e72c9428126b3fb6c23163
IZRINO <sxnull@163.com>
2024-12-18 15:10:03 +0800

    Initialize tauri-shadcn-vue

---
+++ README.md
@@ -0,0 +1,11 @@
+# Astro + Vue Example
+这是一个Astro + Vue + shadcn-vue + Tauri的初始模板
+
+This is an initial template for Astro + Vue + shadcn-vue + Tauri.
+
+使用此模板:
+
+Using this template:
+```sh
+pnpm create astro@latest --template IZRINO/astro-vue-template#tauri-shadcn-vue
+```
---
+++ src/components/Counter.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Button } from "$/components/ui/button"
+
+import { ref } from 'vue';
+
+const count = ref(0);
+const add = () => count.value++;
+const subtract = () => count.value--;
+</script>
+
+<template>
+	<div class="counter">
+		<Button @click="subtract">-</Button>
+		<pre>{{ count }}</pre>
+		<Button @click="add">+</Button>
+	</div>
+	<div class="counter-message">
+		<slot />
+	</div>
+</template>
+
+<style>
+.counter {
+	display: grid;
+	font-size: 2em;
+	grid-template-columns: repeat(3, minmax(0, 1fr));
+	margin-top: 2em;
+	place-items: center;
+}
+
+.counter-message {
+	text-align: center;
+}
+</style>
