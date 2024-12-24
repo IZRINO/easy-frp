a6fddbefd3b7656ee9814f8c561403b1a4b5b3dd
IZRINO <sxnull@163.com>
2024-12-24 17:33:41 +0800

    Refactor Astro configuration: Update integration settings and build options
    - Standardized import statements to use double quotes for consistency.
    - Added base path and trailing slash settings to the Astro configuration.
    - Enhanced integration setup for Vue and Tailwind, ensuring proper support for Vue components.
    - Introduced a new build format option to generate HTML files without index paths.
    - Included comments in Chinese to clarify build process adjustments.
    
    These changes improve the configuration clarity and enhance the build process for the Astro project.

--- Service/web/astro.config.mjs
+++ Service/web/astro.config.mjs
@@ -1,13 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    // Enable Vue to support Vue components.
-    integrations: [vue(), tailwind({
-        applyBaseStyles: false,
-      })],
+	// Enable Vue to support Vue components.
+	base: "/",
+	integrations: [
+		vue(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	trailingSlash: "never",
+	build: {
+		// 示例：在构建过程中生 成`page.html` 而不是 `page/index.html`。
+		format: "file",
+	},
 });
