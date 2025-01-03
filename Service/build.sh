781b549fa241b872e6f9f4104596eac4eb590353
IZRINO <sxnull@163.com>
2025-01-03 17:31:57 +0800

    Refactor build process: Enhance build.sh and Makefile for multi-platform support
    - Updated build.sh to allow specifying target platforms and architectures via script parameters.
    - Improved Makefile to include 'darwin' as a target platform and added conditional logic to skip 'darwin-386' builds.
    - Cleaned up the build process by ensuring proper removal of previous build artifacts and organizing the build commands.
    
    These changes streamline the build process, making it more flexible and accommodating for different environments.

--- Service/build.sh
+++ Service/build.sh
@@ -1,9 +1,39 @@
+#!/bin/bash
+
+# 默认目标平台和架构
+TARGETS="windows linux darwin"
+ARCHS="amd64 arm64 386"
+
+# 解析脚本参数
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -t=*)
+      TARGETS="${1#*=}"
+      shift
+      ;;
+    -a=*)
+      ARCHS="${1#*=}"
+      shift
+      ;;
+    *)
+      echo "Unknown parameter: $1"
+      exit 1
+      ;;
+  esac
+done
+
 make clean
+
 cd ./web
+sudo rm -rf .astro
+sudo rm -rf node_modules
+
 pnpm i
 pnpm build
-rm -rf ../dist 
-mv -f dist/ ../
+sudo rm -rf ../dist 
+sudo mv -f dist/ ../
 cd ../
-make build
-rm -rf dist
+
+# 传递目标平台和架构给Makefile
+make build TARGETS="$TARGETS" ARCHS="$ARCHS"
+sudo rm -rf dist
