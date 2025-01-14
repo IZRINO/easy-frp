5193da02cbb96b8bda018c4e206fa2297c8d4bc6
IZRINO <sxnull@163.com>
2025-01-14 17:25:33 +0800

    去掉未使用的方法

--- Service/main.go
+++ Service/main.go
@@ -4,7 +4,6 @@ import (
 	"embed"
 	"flag"
 	"fmt"
-	"net"
 	"net/http"
 	"os"
 
@@ -43,21 +42,4 @@ func main() {
 
 	//保持运行
 	select {}
-}
-
-// 获取本地IP地址
-func getLocalIP() (string, error) {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		return "", err
-	}
-	for _, address := range addrs {
-		// 检查ip地址判断是否回环地址
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String(), nil
-			}
-		}
-	}
-	return "", fmt.Errorf("无法获取本地IP地址")
 }
